2a39e6cb95f1276498aaf497bbee0d0d4ba21cbb
azulkipli <azulkipli@gmail.com>
2023-10-09 20:57:09 +0700

    setting apps & packages

---
+++ apps/web1/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+   <h1>Welcome to Web1</h1>
+   <nx3-btn>btn 1</nx3-btn>
+  </div>
+</template>
---
+++ packages/nuxt3-web-pkg/lib/components/Btn.vue
@@ -0,0 +1,16 @@
+<template>
+    <button>
+        <slot></slot>
+    </button>
+</template>
+
+<style  scoped>
+button { 
+    border: none;
+    padding: 10px 20px;
+    background: rgb(35, 35, 105);
+    color: white;
+    border-radius: 5px;
+    /* some other stuff */
+}
+</style>
