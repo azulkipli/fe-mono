93e5d37e4c44d2cba66ac0201ceb523fff25c1b7
azulkipli <azulkipli@gmail.com>
2023-11-17 18:35:13 +0700

    prettier-plugin-tailwindcss

--- apps/espretapivi/pages/about.vue
+++ apps/espretapivi/pages/about.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="pageAbout" class="px-2">
     <h1>About</h1>
+    <div class="gap flex justify-between bg-slate-200">
+      <div class="m-2 my-1 p-4 pt-2 text-red-500">kolom 1</div>
+      <div class="text-greeb-500">kolom 2</div>
+      3
+    </div>
   </div>
 </template>
