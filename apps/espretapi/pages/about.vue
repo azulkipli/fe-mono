fec68daa4b74fa6a00aaaf8f8d0f676217808c6f
azulkipli <azulkipli@gmail.com>
2023-11-17 20:19:12 +0700

    rename to espretapi

---
+++ apps/espretapi/pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="pageAbout" class="px-2">
+    <h1>About</h1>
+    <div class="gap flex justify-between border bg-slate-100">
+      <div class="m-2 my-1 p-4 pt-2 text-red-500">column 1</div>
+      <div class="m-2 my-1 p-4 pt-2 text-blue-500">column 2</div>
+    </div>
+  </div>
+</template>
